78a9bf7b678a9f00df7a20339da803e465cd823b
JonLec2 <92182897+JonLec2@users.noreply.github.com>
2022-01-07 17:20:01 -0600

    Add files via upload

---
+++ Drops.js
@@ -0,0 +1,38 @@
+class Drops{
+constructor(x, y){
+    var options={
+restitution:1.2, 
+friction:0 ,
+
+    }
+
+this.r=5
+this.body=Bodies.circle(x, y, this.r, options);
+World.add(world, this.body);
+
+}
+
+display(){
+    var pos=this.body.position;
+    var angle=this.body.angle;
+
+push();
+translate(pos.x, pos.y);
+rotate(angle);
+
+fill("blue");
+ellipseMode(RADIUS)
+ellipse(0, 0, this.r, this.r);
+pop();
+
+
+}
+
+update(){
+    if(this.body.position.y >height)
+
+    Matter.Body.setPosition(this.body, {x:random(0, 400), y: random(0, 400)})
+}
+
+
+}
